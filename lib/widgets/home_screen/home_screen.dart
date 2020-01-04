c78690d889d64e9aae7c193e951a34ebbcb32344
James Leahy <defuncart@gmail.com>
2020-01-04 15:01:16 +0100

    Update loca.

--- lib/widgets/home_screen/home_screen.dart
+++ lib/widgets/home_screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:adaptive_library/adaptive_library.dart';
 import 'package:flutter/material.dart';
 
 import 'package:chat_messenger/i18n.dart';
+import 'package:chat_messenger/widgets/chat_screen/chat_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -9,12 +10,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AdaptiveScaffold(
-      title: Text(I18n.appTitle),
       appBar: AppBar(
         title: Text(I18n.appTitle),
       ),
-      body: Center(
-        child: Text(I18n.test),
+      body: SafeArea(
+        child: ChatScreen(),
       ),
     );
   }
