3811358a0aed375d1a87e11778d5ca0c345f87ef
James Leahy <defuncart@gmail.com>
2020-01-04 09:20:44 +0100

    Add module chat_service.

---
+++ lib/modules/chat_service/src/services/fake_chat_service.dart
@@ -0,0 +1,10 @@
+import 'i_chat_service.dart';
+
+/// A fake chat service for testing
+class FakeChatService implements IChatService {
+  /// Attempts to log in the user anonymously
+  Future<bool> loginUser() async {
+    Future.delayed(Duration(seconds: 2));
+    return true;
+  }
+}
