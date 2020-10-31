595a5a5faac4c50a25d4d392a2c12dfaeab8a77b
James Leahy <defuncart@gmail.com>
2020-10-31 12:38:25 +0100

    Add logout button to ChatScreen.

--- lib/widgets/chat_screen/chat_screen.dart
+++ lib/widgets/chat_screen/chat_screen.dart
@@ -6,6 +6,7 @@ import 'package:chat_messenger/i18n.dart';
 import 'package:chat_messenger/modules/chat_service/chat_service.dart';
 import 'package:chat_messenger/modules/user_preferences/user_preferences.dart';
 import 'package:chat_messenger/modules/uuid/uuid.dart';
+import 'package:chat_messenger/widgets/create_user_screen/create_user_screen.dart';
 import 'package:dash_chat/dash_chat.dart';
 import 'package:flutter/material.dart';
 import 'package:giphy_picker/giphy_picker.dart';
@@ -53,6 +54,27 @@ class _ChatScreenState extends State<ChatScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(I18n.appTitle),
+        actions: [
+          PopupMenuButton<int>(
+            onSelected: (index) async {
+              await context.read<IChatService>().logoutUser();
+              await UserPreferences.clear();
+              Navigator.of(context).pushReplacement(
+                // platform adaptive route by default
+                MaterialPageRoute(
+                  builder: (context) => CreateUserScreen(),
+                ),
+              );
+            },
+            color: Theme.of(context).scaffoldBackgroundColor,
+            itemBuilder: (_) => [
+              PopupMenuItem<int>(
+                value: 0,
+                child: Text(I18n.generalLogout),
+              )
+            ],
+          ),
+        ],
       ),
       body: SafeArea(
         child: StreamBuilder(
